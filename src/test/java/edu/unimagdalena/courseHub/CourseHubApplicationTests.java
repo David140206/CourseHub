c32b8be94ca45abe90c54ac982990652c0528e4c
David140206 <dbrunal@unimagdalena.edu.co>
2026-09-07 18:46:09 -0500

    Primer commit: vincular proyecto

---
+++ src/test/java/edu/unimagdalena/courseHub/CourseHubApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.unimagdalena.courseHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
